b7ea3fd98ae77e3bbc56152ff08000cca1140276
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-07-16 10:34:34 -0400

    Update rep_finder_pure_js.js

--- system/modules/rep_finder/assets/js/rep_finder_pure_js.js
+++ system/modules/rep_finder/assets/js/rep_finder_pure_js.js
@@ -3,5 +3,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   
   // start our custom code here
+  console.log("Firing after the document is loaded, using just pure JS");
   
 }, false);
